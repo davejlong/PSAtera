7c50f264ac0eff8ec29b24513a72179e1a7f60e2
Dave Long <dlong@cagedata.com>
2020-04-23 22:11:10 -0400

    Adds filtering logic to Alerts and Tickets

--- endpoints/Alerts.ps1
+++ endpoints/Alerts.ps1
@@ -1,6 +1,3 @@
-###
-# Alert Endpoint
-###
 function Get-AteraAlerts {
   return New-GetRequest -Endpoint "/alerts"
 }
@@ -11,4 +8,28 @@ function Get-AteraAlert {
     [int]$ID
   )
   return New-GetRequest -Endpoint "/alerts/$alertId"
+}
+
+function Get-AteraAlertsFiltered {
+  param(
+    # Get Open alerts
+    [switch] $Open,
+    # Get Closed alerts
+    [switch] $Closed,
+    # Get Information alerts
+    [switch] $Information,
+    # Get Warning alerts
+    [switch] $Warning,
+    # Get Critical alerts
+    [switch] $Critical
+  )
+
+  return Get-AteraAlerts | Where-Object {
+    if ($Open.IsPresent -and !$_.Archived) { return $true }
+    if ($Closed.IsPresent -and $_.Archived) { return $true }
+
+    if ($Information.IsPresent -and $_.Severity -eq "Information") { return $true }
+    if ($Warning.IsPresent -and $_.Severity -eq "Warning") { return $true }
+    if ($Critical.IsPresent -and $_.Severity -eq "Critical") { return $true }
+  }
 }
--- endpoints/Tickets.ps1
+++ endpoints/Tickets.ps1
@@ -49,3 +49,23 @@ function Get-AteraTicketComments {
   )
   return New-GetRequest -Endpoint "/tickets/$ID/comments"
 }
+
+function Get-AteraTicketsFiltered {
+  param(
+    # Get Open tickets
+    [switch] $Open,
+    # Get Pending tickets
+    [switch] $Pending,
+    # Get Resolved tickets
+    [switch] $Resolved,
+    # Get Closed tickets
+    [switch] $Closed
+  )
+
+  return Get-AteraTickets | Where-Object {
+    if ($Open.IsPresent -and $_.TicketStatus -eq "Open") { return $true }
+    if ($Pending.IsPresent -and $_.TicketStatus -eq "Pending") { return $true }
+    if ($Resolved.IsPresent -and $_.TicketStatus -eq "Resolved") { return $true }
+    if ($Closed.IsPresent -and $_.TicketStatus -eq "Closed") { return $true }
+  }
+}
