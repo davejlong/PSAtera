c358cb332e0662bfcf09d4a260cef51aa08b6b48
Jacob Ernst <Jacobaernst@gmail.com>
2020-05-07 15:55:39 -0400

    Support getting single customer
    Support getting single customer

--- endpoints/Customers.ps1
+++ endpoints/Customers.ps1
@@ -1,3 +1,11 @@
 function Get-AteraCustomers {
-  return New-GetRequest -Endpoint "/customers"
+  param(
+    [Parameter(Mandatory=$false)][int]$CustomerID
+  )
+  if($CustomerID){
+    return New-GetRequest -Endpoint "/customers/$($CustomerID)"
+  }
+  else{
+    return New-GetRequest -Endpoint "/customers"
+  }
 }
