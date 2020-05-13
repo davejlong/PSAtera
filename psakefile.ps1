2564cdd4400aa0a406cea52585b08c2ba271e64b
Dave Long <dlong@cagedata.com>
2020-05-13 11:36:05 -0400

    Version bump

--- psakefile.ps1
+++ psakefile.ps1
@@ -9,6 +9,7 @@ Task FunctionsToExport {
             $ast.EndBlock.Statements.Name
         }
     }
+    $functionNames += @("Install-AteraAgent", "Get-AteraAPIKey", "Set-AteraAPIKey", "Get-AteraRecordLimit", "Set-AteraRecordLimit")
     Write-Verbose "Using functions $functionNames"
 
     Update-ModuleManifest -Path ".\$($moduleName).psd1" -FunctionsToExport $functionNames
