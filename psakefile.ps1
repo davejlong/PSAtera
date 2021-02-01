15ea74f0cdd48c8210986c118e99900536afaaa3
David Long <dlong@cagedata.com>
2021-02-01 08:58:08 -0500

    Added docs.txt with complete content of extracted help

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,22 +1,32 @@
 Task default -depends FunctionsToExport
 
 Task FunctionsToExport {
-    $moduleName = Get-Item . | ForEach-Object BaseName
-
-    $functionNames = Get-ChildItem endpoints/ -Recurse -PipelineVariable file | ForEach-Object {
-        $ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref] $null, [ref] $null)
-        if ($ast.EndBlock.Statements.Name) {
-            $ast.EndBlock.Statements.Name
-        }
+  $moduleName = Get-Item . | ForEach-Object BaseName
+  
+  $functionNames = Get-ChildItem endpoints/ -Recurse -PipelineVariable file | ForEach-Object {
+    $ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref] $null, [ref] $null)
+    if ($ast.EndBlock.Statements.Name) {
+      $ast.EndBlock.Statements.Name
     }
-    $functionNames += @("Install-AteraAgent", "Get-AteraAPIKey", "Set-AteraAPIKey", "Get-AteraRecordLimit", "Set-AteraRecordLimit", "New-AteraGetRequest", "New-AteraPostRequest")
-    Write-Verbose "Using functions $functionNames"
-
-    Update-ModuleManifest -Path ".\$($moduleName).psd1" -FunctionsToExport $functionNames
-    Import-Module $moduleName -Force -Verbose:$false
+  }
+  $functionNames += @("Install-AteraAgent", "Get-AteraAPIKey", "Set-AteraAPIKey", "Get-AteraRecordLimit", "Set-AteraRecordLimit", "New-AteraGetRequest", "New-AteraPostRequest")
+  Write-Verbose "Using functions $functionNames"
+  
+  Update-ModuleManifest -Path ".\$($moduleName).psd1" -FunctionsToExport $functionNames
+  Import-Module $moduleName -Force -Verbose:$false
 }
 
 Task Publish -depends FunctionsToExport {
-    $moduleName = Get-Item . | ForEach-Object BaseName
-    Publish-Module -Name ".\$($moduleName).psm1" -NuGetApiKey $env:NuGetAPIKey
+  $moduleName = Get-Item . | ForEach-Object BaseName
+  Publish-Module -Name ".\$($moduleName).psm1" -NuGetApiKey $env:NuGetAPIKey
+}
+
+Task ExtractDocs {
+  Remove-Item "./docs.txt"
+  $moduleName = Get-Item . | ForEach-Object BaseName
+  Import-Module -Name "./$($moduleName).psm1" -Force
+  
+  Get-Command -Module $moduleName | ForEach-Object {
+    Get-Help $_.Name -Detailed | Out-File -FilePath "./docs.txt" -Append
+  }
 }
